de6624b9515a761aa8caecac055348e8fa8048a5
Simone Rossi <simone.rossi.93@gmail.com>
2016-06-09 11:58:05 +0200

    Modified port names

--- 0_MISC/register.vhd
+++ 0_MISC/register.vhd
@@ -1,34 +1,34 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity register is
+entity REGISTER is
 	generic(
-		NBIT: integer := 4
+		RGS_NBIT: integer := 4
 	);
 	port(
-		d	:	in	std_logic_vector(NBIT-1 downto 0);
-		ck 	:	in	std_logic;
-		reset 	:	in	std_logic;
-		q	:	out	std_logic_vector(NBIT-1 downto 0)
+		RGS_D	:	in	std_logic_vector(RGS_NBIT-1 downto 0);
+		CLK 	:	in	std_logic;
+		RST 	:	in	std_logic;
+		RGS_Q	:	out	std_logic_vector(RGS_NBIT-1 downto 0)
 	);
-end register;
+end REGISTER;
 
-architecture BHV of register is
+architecture BHV of REGISTER is
 begin
-	process(ck)
+	process(CLK)
 	begin
-    if(ck'event and ck = '1') then
-      if(reset = '1') then
-        q <= (others => '0');
+    if(CLK'event and CLK = '1') then
+      if(RST = '1') then
+        RGS_Q <= (others => '0');
       else
-        q <= d;
+        RGS_Q <= RGS_D;
       end if;
     end if;
 	end process;
 end BHV;
 
 
-configuration CFG_register_BEH_SYNC of register is
+configuration CFG_REGISTER_BHV of REGISTER is
 	for BHV
 	end for;
-end CFG_register_BEH_SYNC;
+end CFG_REGISTER_BHV;
