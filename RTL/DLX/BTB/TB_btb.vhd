03f1f8f3f60b3c919dec6709e1f7575ff340d474
Simone Rossi <simone.rossi.93@gmail.com>
2016-07-29 17:40:22 +0200

    .

--- RTL/DLX/BTB/TB_btb.vhd
+++ RTL/DLX/BTB/TB_btb.vhd
@@ -3,7 +3,6 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity TB_btb is
-
 end entity TB_btb;
 
 architecture TEST of TB_btb is
@@ -11,7 +10,7 @@ architecture TEST of TB_btb is
     generic(
       BTB_NBIT    : integer := 32;
       BTB_ENTRIES : integer := 1024
-      );
+    );
     port(
       BTB_PC                   : in  std_logic_vector(BTB_NBIT - 1 downto 0);
       BTB_TARGET_PC            : out std_logic_vector(BTB_NBIT - 1 downto 0);
@@ -23,24 +22,54 @@ architecture TEST of TB_btb is
       BTB_EN                   : in  std_logic;
       BTB_RST                  : in  std_logic;
       BTB_CLK                  : in  std_logic
-      );
+    );
   end component BTB;
+
+  constant c_NBIT    : integer := 32;
+  constant c_ENTRIES : integer := 4;
+  constant c_TCLK    : time    := 1 ns;
+
+  signal s_CALCULATED_PC     : std_logic_vector(c_NBIT - 1 downto 0) := (others => '0');
+  signal s_CALCULATED_TARGET : std_logic_vector(c_NBIT - 1 downto 0) := (others => '0');
+  signal s_PC                : std_logic_vector(c_NBIT - 1 downto 0) := (others => '0');
+  signal s_TARGET_PC         : std_logic_vector(c_NBIT - 1 downto 0) := (others => '0');
+
+  signal s_BRANCH_PREDICTION    : std_logic := '0';
+  signal s_CALCULATED_CONDITION : std_logic := '0';
+  signal s_CLR                  : std_logic := '0';
+  signal s_EN                   : std_logic := '0';
+  signal s_RST                  : std_logic := '0';
+  signal s_CLK                  : std_logic := '0';
+
 begin
   UUT : component BTB
     generic map(
-      BTB_NBIT    => 32,
-      BTB_ENTRIES => 1024
-      )
+      BTB_NBIT    => c_NBIT,
+      BTB_ENTRIES => c_ENTRIES
+    )
     port map(
-      BTB_PC                   => (others => '0'),
-      BTB_TARGET_PC            => open,
-      BTB_BRANCH_PREDICTION    => open,
-      BTB_CALCULATED_PC        => (others => '0'),
-      BTB_CALCULATED_TARGET    => (others => '0'),
-      BTB_CALCULATED_CONDITION => '0',
-      BTB_CLR                  => '0',
-      BTB_EN                   => '0',
-      BTB_RST                  => '0',
-      BTB_CLK                  => '0'
-      );
+      BTB_PC                   => s_PC,
+      BTB_TARGET_PC            => s_TARGET_PC,
+      BTB_BRANCH_PREDICTION    => s_BRANCH_PREDICTION,
+      BTB_CALCULATED_PC        => s_CALCULATED_PC,
+      BTB_CALCULATED_TARGET    => s_CALCULATED_TARGET,
+      BTB_CALCULATED_CONDITION => s_CALCULATED_CONDITION,
+      BTB_CLR                  => s_CLR,
+      BTB_EN                   => s_EN,
+      BTB_RST                  => s_RST,
+      BTB_CLK                  => s_CLK
+    );
+
+  CLK_PROC : process
+  begin
+    wait for c_TCLK / 2;
+    s_CLK <= not s_CLK;
+  end process CLK_PROC;
+
+  STIM : process
+  begin
+    wait for 
+    wait;
+  end process STIM;
+
 end architecture TEST;
