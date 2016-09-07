9fb25a025fbbbc5a3ee62ca342bfb5c2c4ade936
Simone Rossi <simone.rossi.93@gmail.com>
2016-09-07 17:15:33 +0200

    Added more logic for memory

--- RTL/DLX/CORE/4_MEMORY/TB_memory.vhd
+++ RTL/DLX/CORE/4_MEMORY/TB_memory.vhd
@@ -7,61 +7,103 @@ end entity TB_memory;
 
 architecture TEST of TB_memory is
   component memory
-    generic(MEM_NBIT : integer := 32);
+    generic(
+      MEM_IR_NBIT   : integer := 32;
+      MEM_PC_NBIT   : integer := 32;
+      MEM_DATA_NBIT : integer := 32;
+      MEM_ADDR_NBIT : integer := 32
+    );
     port(
-      MEM_IR_IN           : in    std_logic_vector(MEM_NBIT - 1 downto 0);
-      MEM_NPC_IN          : in    std_logic_vector(MEM_NBIT - 1 downto 0);
-      MEM_IR_OUT          : out   std_logic_vector(MEM_NBIT - 1 downto 0);
-      MEM_NPC_OUT         : out   std_logic_vector(MEM_NBIT - 1 downto 0);
-      MEM_ADDRESS_IN      : in    std_logic_vector(MEM_NBIT - 1 downto 0);
-      MEM_ADDRESS_OUT     : out   std_logic_vector(MEM_NBIT - 1 downto 0);
-      MEM_INTERFACE       : inout std_logic_vector(MEM_NBIT - 1 downto 0);
-      MEM_DATA_IN         : in    std_logic_vector(MEM_NBIT - 1 downto 0);
+      MEM_IR_IN           : in    std_logic_vector(MEM_IR_NBIT - 1 downto 0);
+      MEM_NPC_IN          : in    std_logic_vector(MEM_PC_NBIT - 1 downto 0);
+      MEM_IR_OUT          : out   std_logic_vector(MEM_IR_NBIT - 1 downto 0);
+      MEM_NPC_OUT         : out   std_logic_vector(MEM_PC_NBIT - 1 downto 0);
+      MEM_ADDRESS_IN      : in    std_logic_vector(MEM_ADDR_NBIT - 1 downto 0);
+      MEM_ADDRESS_OUT     : out   std_logic_vector(MEM_ADDR_NBIT - 1 downto 0);
+      MEM_DATA_IN         : in    std_logic_vector(MEM_DATA_NBIT - 1 downto 0);
       MEM_CU_READNOTWRITE : in    std_logic;
-      MEM_DATA_OUT        : out   std_logic_vector(MEM_NBIT - 1 downto 0)
+      MEM_DATA_OUT        : out   std_logic_vector(MEM_DATA_NBIT - 1 downto 0);
+      MEM_INTERFACE       : inout std_logic_vector(MEM_DATA_NBIT - 1 downto 0);
+      MEM_CU_SIGNED_LOAD  : in    std_logic;
+      MEM_CU_LOAD_TYPE    : in    std_logic_vector(1 downto 0)
     );
   end component memory;
-  constant c_NBIT : integer := 32;
+
+  constant c_NBIT          : integer                               := 32;
   signal s_IR_IN           : std_logic_vector(c_NBIT - 1 downto 0) := (others => '0');
   signal s_NPC_IN          : std_logic_vector(c_NBIT - 1 downto 0) := (others => '0');
   signal s_IR_OUT          : std_logic_vector(c_NBIT - 1 downto 0);
   signal s_NPC_OUT         : std_logic_vector(c_NBIT - 1 downto 0);
   signal s_ADDRESS_IN      : std_logic_vector(c_NBIT - 1 downto 0) := (others => '0');
   signal s_ADDRESS_OUT     : std_logic_vector(c_NBIT - 1 downto 0);
-  signal s_INTERFACE       : std_logic_vector(c_NBIT - 1 downto 0) :=(others => '1');
+  signal s_INTERFACE       : std_logic_vector(c_NBIT - 1 downto 0) := (others => '0');
   signal s_DATA_IN         : std_logic_vector(c_NBIT - 1 downto 0) := (others => '0');
-  signal s_CU_READNOTWRITE : std_logic := '1';       
+  signal s_CU_READNOTWRITE : std_logic                             := '0';
   signal s_DATA_OUT        : std_logic_vector(c_NBIT - 1 downto 0);
-begin
-UUT : memory
-  generic map(
-    MEM_NBIT => c_NBIT
-  )
-  port map(
-    MEM_IR_IN           => s_IR_IN,
-    MEM_NPC_IN          => s_NPC_IN,
-    MEM_IR_OUT          => s_IR_OUT,
-    MEM_NPC_OUT         => s_NPC_OUT,
-    MEM_ADDRESS_IN      => s_ADDRESS_IN,
-    MEM_ADDRESS_OUT     => s_ADDRESS_OUT,
-    MEM_INTERFACE       => s_INTERFACE,
-    MEM_DATA_IN         => s_DATA_IN,
-    MEM_CU_READNOTWRITE => s_CU_READNOTWRITE,
-    MEM_DATA_OUT        => s_DATA_OUT
-  );
 
-process
+  signal s_CU_SIGNED_LOAD : std_logic                    := '0';
+  signal s_CU_LOAD_TYPE   : std_logic_vector(1 downto 0) := "00";
 begin
-wait for 1 ns;
--- Read from memory
-s_CU_READNOTWRITE <= '1';
-s_INTERFACE <= x"AAAAAAAA";
-wait for 1 ns;
--- Write in memory
-s_INTERFACE <= (others => 'Z');
-s_DATA_IN <= x"EEEEEEEE";
-s_CU_READNOTWRITE <= '0';x
-wait;
-end process;
+  UUT : memory
+    generic map(
+      MEM_IR_NBIT   => c_NBIT,
+      MEM_PC_NBIT   => c_NBIT,
+      MEM_DATA_NBIT => c_NBIT,
+      MEM_ADDR_NBIT => c_NBIT
+    )
+    port map(
+      MEM_IR_IN           => s_IR_IN,
+      MEM_NPC_IN          => s_NPC_IN,
+      MEM_IR_OUT          => s_IR_OUT,
+      MEM_NPC_OUT         => s_NPC_OUT,
+      MEM_ADDRESS_IN      => s_ADDRESS_IN,
+      MEM_ADDRESS_OUT     => s_ADDRESS_OUT,
+      MEM_INTERFACE       => s_INTERFACE,
+      MEM_DATA_IN         => s_DATA_IN,
+      MEM_CU_READNOTWRITE => s_CU_READNOTWRITE,
+      MEM_DATA_OUT        => s_DATA_OUT,
+      MEM_CU_SIGNED_LOAD  => s_CU_SIGNED_LOAD,
+      MEM_CU_LOAD_TYPE    => s_CU_LOAD_TYPE
+    );
+
+  process
+  begin
+    -- Write in memory
+    s_INTERFACE       <= (others => 'Z');
+    s_DATA_IN         <= x"EEEEEEEE";
+    s_CU_READNOTWRITE <= '0';
+    wait for 1 ns;
+    -- Read unsigned byte
+    s_INTERFACE <= x"000000FF";
+    s_CU_LOAD_TYPE <= "10";
+    s_CU_SIGNED_LOAD <= '0';
+    s_CU_READNOTWRITE <= '1';
+    wait for 1 ns;
+    -- Read signed byte
+    s_INTERFACE <= x"000000FF";
+    s_CU_LOAD_TYPE <= "10";
+    s_CU_SIGNED_LOAD <= '1';
+    s_CU_READNOTWRITE <= '1';
+    wait for 1 ns;
+    -- Read unsigned halfword
+    s_INTERFACE <= x"0000FFFF";
+    s_CU_LOAD_TYPE <= "01";
+    s_CU_SIGNED_LOAD <= '0';
+    s_CU_READNOTWRITE <= '1';
+    wait for 1 ns;
+    -- Read signed halfword
+    s_INTERFACE <= x"0000FFFF";
+    s_CU_LOAD_TYPE <= "01";
+    s_CU_SIGNED_LOAD <= '1';
+    s_CU_READNOTWRITE <= '1';
+    wait for 1 ns;
+    -- Read word
+    s_INTERFACE <= x"FFFFFFFF";
+    s_CU_LOAD_TYPE <= "00";
+    s_CU_SIGNED_LOAD <= '0';
+    s_CU_READNOTWRITE <= '1';
+    wait for 1 ns;
+    wait;
+  end process;
 
 end architecture TEST;
--- RTL/DLX/CORE/4_MEMORY/memory.vhd
+++ RTL/DLX/CORE/4_MEMORY/memory.vhd
@@ -4,36 +4,116 @@ use ieee.numeric_std.all;
 
 entity memory is
   generic(
-    MEM_NBIT : integer := 32);
+    MEM_IR_NBIT   : integer := 32;
+    MEM_PC_NBIT   : integer := 32;
+    MEM_DATA_NBIT : integer := 32;
+    MEM_ADDR_NBIT : integer := 32
+  );
   port(
-    MEM_IR_IN  : in std_logic_vector(MEM_NBIT - 1 downto 0);  -- Instruction register in 
-    MEM_NPC_IN : in std_logic_vector(MEM_NBIT - 1 downto 0);  -- Next program counter (it can be the speculated) 
+    MEM_IR_IN           : in    std_logic_vector(MEM_IR_NBIT - 1 downto 0); -- Instruction register in 
+    MEM_NPC_IN          : in    std_logic_vector(MEM_PC_NBIT - 1 downto 0); -- Next program counter (it can be the speculated) 
 
-    MEM_IR_OUT  : out std_logic_vector(MEM_NBIT - 1 downto 0);  -- Instruction register out 
-    MEM_NPC_OUT : out std_logic_vector(MEM_NBIT - 1 downto 0);  -- Next program counter (it can be the speculated) 
+    MEM_IR_OUT          : out   std_logic_vector(MEM_IR_NBIT - 1 downto 0); -- Instruction register out 
+    MEM_NPC_OUT         : out   std_logic_vector(MEM_PC_NBIT - 1 downto 0); -- Next program counter (it can be the speculated) 
 
-    MEM_ADDRESS_IN      : in    std_logic_vector(MEM_NBIT - 1 downto 0);
-    MEM_ADDRESS_OUT     : out   std_logic_vector(MEM_NBIT - 1 downto 0);
-    MEM_INTERFACE       : inout std_logic_vector(MEM_NBIT - 1 downto 0);
-    MEM_DATA_IN         : in    std_logic_vector(MEM_NBIT - 1 downto 0);
-    MEM_CU_READNOTWRITE : in    std_logic;
-    MEM_DATA_OUT        : out   std_logic_vector(MEM_NBIT - 1 downto 0)
-    );
+    MEM_ADDRESS_IN      : in    std_logic_vector(MEM_ADDR_NBIT - 1 downto 0); --
+    MEM_ADDRESS_OUT     : out   std_logic_vector(MEM_ADDR_NBIT - 1 downto 0); --
+
+    MEM_DATA_IN         : in    std_logic_vector(MEM_DATA_NBIT - 1 downto 0); --
+    MEM_CU_READNOTWRITE : in    std_logic; --
+    MEM_DATA_OUT        : out   std_logic_vector(MEM_DATA_NBIT - 1 downto 0); --
+    MEM_INTERFACE       : inout std_logic_vector(MEM_DATA_NBIT - 1 downto 0); --
+
+    MEM_CU_SIGNED_LOAD  : in    std_logic;
+    MEM_CU_LOAD_TYPE    : in    std_logic_vector(1 downto 0)
+  );
 end entity memory;
 
 architecture STR of memory is
-  signal s_internal_ir      : std_logic_vector(MEM_NBIT - 1 downto 0);
-  signal s_internal_npc     : std_logic_vector(MEM_NBIT - 1 downto 0);
-  signal s_internal_address : std_logic_vector(MEM_NBIT - 1 downto 0);
+  component sign_extention
+    generic(
+      SIGN_EXT_IN_NBIT  : integer := 16;
+      SIGN_EXT_OUT_NBIT : integer := 32
+    );
+    port(
+      SIGN_EXT_signed : in  std_logic;
+      SIGN_EXT_input  : in  std_logic_vector(SIGN_EXT_IN_NBIT - 1 downto 0);
+      SIGN_EXT_output : out std_logic_vector(SIGN_EXT_OUT_NBIT - 1 downto 0)
+    );
+  end component sign_extention;
 
-  -- signal s_internal_data : std_logic_vector(MEM_NBIT-1 downto 0);
+  component mux_4to1
+    generic(MUX_4to1_NBIT : integer := 4);
+    port(
+      MUX_4to1_in0 : in  std_logic_vector(MUX_4to1_NBIT - 1 downto 0);
+      MUX_4to1_in1 : in  std_logic_vector(MUX_4to1_NBIT - 1 downto 0);
+      MUX_4to1_in2 : in  std_logic_vector(MUX_4to1_NBIT - 1 downto 0);
+      MUX_4to1_in3 : in  std_logic_vector(MUX_4to1_NBIT - 1 downto 0);
+      MUX_4to1_sel : in  std_logic_vector(1 downto 0);
+      MUX_4to1_out : out std_logic_vector(MUX_4to1_NBIT - 1 downto 0)
+    );
+  end component mux_4to1;
 
-  -- signal a : std_logic_vector (MEM_NBIT -1 downto 0);   -- DFF that stores 
-  -- value from input.
-  -- signal b : std_logic_vector (MEM_NBIT - 1 downto 0);  -- DFF that stores 
-  -- feedback value.
-  
+  signal s_internal_ir            : std_logic_vector(MEM_IR_NBIT - 1 downto 0);
+  signal s_internal_npc           : std_logic_vector(MEM_PC_NBIT - 1 downto 0);
+  signal s_internal_address       : std_logic_vector(MEM_ADDR_NBIT - 1 downto 0);
+  signal s_internal_data_from_mem : std_logic_vector(MEM_DATA_NBIT - 1 downto 0);
+
+  signal s_internal_data_word     : std_logic_vector(MEM_DATA_NBIT - 1 downto 0);
+  signal s_internal_data_halfword : std_logic_vector(MEM_DATA_NBIT - 1 downto 0);
+  signal s_internal_data_byte     : std_logic_vector(MEM_DATA_NBIT - 1 downto 0);
+
 begin
+  process(MEM_CU_READNOTWRITE, MEM_INTERFACE, MEM_DATA_IN)
+  begin
+    if (MEM_CU_READNOTWRITE = '1') then
+      s_internal_data_from_mem <= MEM_INTERFACE;
+      MEM_INTERFACE            <= (others => 'Z');
+    else
+      if (MEM_CU_READNOTWRITE = '0') then
+        MEM_DATA_OUT  <= MEM_INTERFACE;
+        MEM_INTERFACE <= MEM_DATA_IN;
+      end if;
+    end if;
+  end process;
+
+  SIGN_BYTE : sign_extention
+    generic map(
+      SIGN_EXT_IN_NBIT  => 8,
+      SIGN_EXT_OUT_NBIT => MEM_DATA_NBIT
+    )
+    port map(
+      SIGN_EXT_signed => MEM_CU_SIGNED_LOAD,
+      SIGN_EXT_input  => s_internal_data_from_mem(7 downto 0),
+      SIGN_EXT_output => s_internal_data_byte
+    );
+
+  SIGN_HALFWORD : sign_extention
+    generic map(
+      SIGN_EXT_IN_NBIT  => 16,
+      SIGN_EXT_OUT_NBIT => MEM_DATA_NBIT
+    )
+    port map(
+      SIGN_EXT_signed => MEM_CU_SIGNED_LOAD,
+      SIGN_EXT_input  => s_internal_data_from_mem(15 downto 0),
+      SIGN_EXT_output => s_internal_data_halfword
+    );
+
+  s_internal_data_word <= s_internal_data_from_mem(32 - 1 downto 0);
+
+  MUX_LOAD : component mux_4to1
+    generic map(
+      MUX_4to1_NBIT => MEM_DATA_NBIT
+    )
+    port map(
+      MUX_4to1_in0 => s_internal_data_word,
+      MUX_4to1_in1 => s_internal_data_halfword,
+      MUX_4to1_in2 => s_internal_data_byte,
+      MUX_4to1_in3 => (others => '0'),
+      MUX_4to1_sel => MEM_CU_LOAD_TYPE,
+      MUX_4to1_out => MEM_DATA_OUT
+    );
+
   s_internal_ir <= MEM_IR_IN;
   MEM_IR_OUT    <= s_internal_ir;
 
@@ -43,23 +123,4 @@ begin
   s_internal_address <= MEM_ADDRESS_IN;
   MEM_ADDRESS_OUT    <= s_internal_address;
 
-
-  process (MEM_CU_READNOTWRITE, MEM_INTERFACE, MEM_DATA_IN)
-  begin
-    if(MEM_CU_READNOTWRITE = '1') then
-      MEM_DATA_OUT  <= MEM_INTERFACE;
-      MEM_INTERFACE <= (others => 'Z');
-    else
-
-      if (MEM_CU_READNOTWRITE = '0') then
-        MEM_DATA_OUT  <= MEM_INTERFACE;
-        MEM_INTERFACE <= MEM_DATA_IN;
-      end if;
-
-    end if;
-
-  end process;
-
-
-  
 end architecture STR;
