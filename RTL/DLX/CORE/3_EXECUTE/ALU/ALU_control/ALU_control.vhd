f861c8302ee1af57607dd104f210b9aa8ce44c0a
Simone Rossi <simone.rossi.93@gmail.com>
2016-09-18 16:58:11 +0200

    e

--- RTL/DLX/CORE/3_EXECUTE/ALU/ALU_control/ALU_control.vhd
+++ RTL/DLX/CORE/3_EXECUTE/ALU/ALU_control/ALU_control.vhd
@@ -35,24 +35,24 @@ architecture bhv of ALU_control is
 
 begin
   -- ORIGINAL FROM EMA
-  -- MAIN : process(ALU_CTRL_command)
-  --  begin
-  --      if ALU_CTRL_command(5 downto 4) = "00" then
-  --          ALU_CTRL_res_sel    <=  "00";
-  --          ALU_CTRL_shf_lr     <=  ALU_CTRL_command(3);
-  --          ALU_CTRL_shf_la     <=  ALU_CTRL_command(2);
-  --      elsif ALU_CTRL_command(5 downto 4) = "01" then
-  --          ALU_CTRL_res_sel    <=  "01";
-  --          ALU_CTRL_add_sub    <=  ALU_CTRL_command(3);
-  --      elsif ALU_CTRL_command(5 downto 4) = "10" then
-  --          ALU_CTRL_res_sel    <=  "10";
-  --          ALU_CTRL_comp_sel   <=  ALU_CTRL_command(3 downto 1);
-  --          ALU_CTRL_comp_sign  <=  ALU_CTRL_command(0);
-  --      elsif ALU_CTRL_command(5 downto 4) = "11" then
-  --          ALU_CTRL_res_sel    <=  "11";
-  --          ALU_CTRL_log_func   <=  ALU_CTRL_command(3 downto 0);
-  --      end if;
-  --  end process;
+   MAIN : process(ALU_CTRL_command)
+    begin
+        if ALU_CTRL_command(5 downto 4) = "00" then
+            ALU_CTRL_res_sel    <=  "00";
+            ALU_CTRL_shf_lr     <=  ALU_CTRL_command(3);
+            ALU_CTRL_shf_la     <=  ALU_CTRL_command(2);
+        elsif ALU_CTRL_command(5 downto 4) = "01" then
+            ALU_CTRL_res_sel    <=  "01";
+            ALU_CTRL_add_sub    <=  ALU_CTRL_command(3);
+        elsif ALU_CTRL_command(5 downto 4) = "10" then
+            ALU_CTRL_res_sel    <=  "10";
+            ALU_CTRL_comp_sel   <=  ALU_CTRL_command(3 downto 1);
+            ALU_CTRL_comp_sign  <=  ALU_CTRL_command(0);
+        elsif ALU_CTRL_command(5 downto 4) = "11" then
+            ALU_CTRL_res_sel    <=  "11";
+            ALU_CTRL_log_func   <=  ALU_CTRL_command(3 downto 0);
+        end if;
+    end process;
 
 
   -- MODIFIED BY SIMO (STILL SEQUENTIAL)
@@ -103,31 +103,31 @@ begin
   --end process;
   
   
-  s_control_00 <= ALU_CTRL_command(3) & ALU_CTRL_command(2) & '0' & '0' & "00" & "000" & "0000";
-  s_control_01 <= '0' & '0' & ALU_CTRL_command(3) & '0' & "01" & "000" & "0000";
-  s_control_10 <= '0' & '0' & '0' & ALU_CTRL_command(0) & "10" & ALU_CTRL_command(3 downto 1) & "0000";
-  s_control_11 <= '0' & '0' & '0' & '0' & "11" & "000" & ALU_CTRL_command(3 downto 0);
+ -- s_control_00 <= ALU_CTRL_command(3) & ALU_CTRL_command(2) & '0' & '0' & "00" & "000" & "0000";
+ -- s_control_01 <= '0' & '0' & ALU_CTRL_command(3) & '0' & "01" & "000" & "0000";
+ -- s_control_10 <= '0' & '0' & '0' & ALU_CTRL_command(0) & "10" & ALU_CTRL_command(3 downto 1) & "0000";
+ -- s_control_11 <= '0' & '0' & '0' & '0' & "11" & "000" & ALU_CTRL_command(3 downto 0);
 
-  MUX : mux_4to1
-    generic map(
-      MUX_4to1_NBIT => 13
-    )
-    port map(
-      MUX_4to1_in0 => s_control_00,
-      MUX_4to1_in1 => s_control_01,
-      MUX_4to1_in2 => s_control_10,
-      MUX_4to1_in3 => s_control_11,
-      MUX_4to1_sel => ALU_CTRL_command(5 downto 4),
-      MUX_4to1_out => s_out
-    );
-
-  ALU_CTRL_shf_lr    <= s_out(12);
-  ALU_CTRL_shf_la    <= s_out(11);
-  ALU_CTRL_add_sub   <= s_out(10);
-  ALU_CTRL_comp_sign <= s_out(9);
-  ALU_CTRL_res_sel   <= s_out(8 downto 7);
-  ALU_CTRL_comp_sel  <= s_out(6 downto 4);
-  ALU_CTRL_log_func  <= s_out(3 downto 0);
+ -- MUX : mux_4to1
+ --   generic map(
+ --     MUX_4to1_NBIT => 13
+ --   )
+ --   port map(
+ --     MUX_4to1_in0 => s_control_00,
+ --     MUX_4to1_in1 => s_control_01,
+ --     MUX_4to1_in2 => s_control_10,
+ --     MUX_4to1_in3 => s_control_11,
+ --     MUX_4to1_sel => ALU_CTRL_command(5 downto 4),
+ --     MUX_4to1_out => s_out
+ --   );
+ --
+ -- ALU_CTRL_shf_lr    <= s_out(12);
+ -- ALU_CTRL_shf_la    <= s_out(11);
+ -- ALU_CTRL_add_sub   <= s_out(10);
+ -- ALU_CTRL_comp_sign <= s_out(9);
+ -- ALU_CTRL_res_sel   <= s_out(8 downto 7);
+ -- ALU_CTRL_comp_sel  <= s_out(6 downto 4);
+ -- ALU_CTRL_log_func  <= s_out(3 downto 0);
 
 end bhv;
 
